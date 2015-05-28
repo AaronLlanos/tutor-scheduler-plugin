a025e8b6aafd880c5d33141fa21e14e272a17108
Aaron Llanos <aaronllanos@gmail.com>
2015-05-28 15:58:47 -0600

    Add plugin to WP

--- tutor-appointment-scheduler.php
+++ tutor-appointment-scheduler.php
@@ -8,20 +8,20 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
+ * @link              http://ddce.utexas.edu/academiccenter/gateway-scholars/
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           Tutor Appointment Scheduler
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       Tutor Appointment Scheduler
+ * Plugin URI:        https://github.com/AaronLlanos/tutor-scheduler-plugin.git
+ * Description:       This plugin is proprietery software used by the Longhorn Center for Academic Excellence to schedule tutors of various subjects within the office.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
+ * Author:            Aaron Llanos
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       Tutor Appointment Scheduler
  * Domain Path:       /languages
  */
 
