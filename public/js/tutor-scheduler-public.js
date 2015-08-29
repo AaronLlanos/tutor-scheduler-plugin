617bfdba930daa9db24ff4b995e71c4c9f2db4a4
Aaron Llanos <aaronllanos@gmail.com>
2015-08-29 17:25:57 -0500

    Refactor js for readability

--- public/js/tutor-scheduler-public.js
+++ public/js/tutor-scheduler-public.js
@@ -1,33 +1,55 @@
 (function( $ ) {
 	'use strict';
 
+	//Course input functions
+	var CourseFilters = {
+		bindCourseInput: function () {
+			// $("#course-select").on()
+		}
+	}
+	
+
+	//Fullcalendar functions
+	var FullCalendarFrontEnd = {
+		loadCalendar: function () {
+			var self = this;
+			$('#fullcalendar-frontend').fullCalendar({
+		        editable: false,
+		        allDaySlot: false,
+		        header: {
+				    left:   'prev,next today',
+				    center: 'title',
+				    right:  'month,agendaWeek,agendaDay'
+				},
+				events: eventJSON,
+		        height: "auto",
+		        minTime: "12:00:00",
+		        maxTime: "19:00:00",
+		        timezone: "America/Chicago",
+		        defaultView: 'agendaWeek',
+		        //Drill down view. Month  view to day view, week view to day view 
+		        dayClick: function(date, jsEvent, view) {
+		        	self.drilldown();
+			    },
+		        //Tooltip to confirm appointment here.
+			    eventClick: function(calEvent, jsEvent, view) {
+		        	// Must add a tooltip here
+		        	self.confirmAppointment();
+			    }
+		    });
+		},
+		drilldown: function () {
+
+		},
+		confirmAppointment: function () {
+
+		}
+	}
+
+	//Window load!
 	$( window ).load(function(){
-		$('#fullcalendar-frontend').fullCalendar({
-	        // put your options and callbacks here
-	        editable: false,
-	        allDaySlot: false,
-	        header: {
-			    left:   'prev,next today',
-			    center: 'title',
-			    right:  'month,agendaWeek,agendaDay'
-			},
-			events: eventJSON,
-	        height: "auto",
-	        minTime: "12:00:00",
-	        maxTime: "19:00:00",
-	        timezone: "America/Chicago",
-	        defaultView: 'agendaWeek',
-	        dayClick: function(date, jsEvent, view) {
-	        	// self.addNewEvent(date, 0);
-		    },
-		    eventClick: function(calEvent, jsEvent, view) {
-		    	// self.deleteEvent(calEvent);
-		    }
-	    });
+		FullCalendarFrontEnd.loadCalendar();
 
-	    $('#fullcalendar-frontend').fullCalendar( 'addEventSource', eventJSON);
-
-	    console.log(eventJSON);
 	});
 
 })( jQuery );
