da19fb9e50993ced3b70f12d0fd82dcc0bcba864
Aaron Llanos <aaronllanos@gmail.com>
2015-08-29 18:28:20 -0500

    Add autocomplete functionality

--- public/js/tutor-scheduler-public.js
+++ public/js/tutor-scheduler-public.js
@@ -5,6 +5,14 @@
 	var CourseFilters = {
 		bindCourseInput: function () {
 			// $("#course-select").on()
+			var courseNamesList = [];
+			$.each(coursesJSON, function (i, course) {
+				console.log(course);
+				courseNamesList.push(course.name);
+			});
+			$("#course-select").autocomplete({
+				source: courseNamesList
+			});
 		}
 	}
 	
@@ -13,7 +21,7 @@
 	var FullCalendarFrontEnd = {
 		loadCalendar: function () {
 			var self = this;
-			$('#fullcalendar-frontend').fullCalendar({
+			$("#fullcalendar-frontend").fullCalendar({
 		        editable: false,
 		        allDaySlot: false,
 		        header: {
@@ -22,9 +30,10 @@
 				    right:  'month,agendaWeek,agendaDay'
 				},
 				events: eventJSON,
+				slotEventOverlap: true,
 		        height: "auto",
 		        minTime: "12:00:00",
-		        maxTime: "19:00:00",
+		        maxTime: "20:00:00",
 		        timezone: "America/Chicago",
 		        defaultView: 'agendaWeek',
 		        //Drill down view. Month  view to day view, week view to day view 
@@ -38,9 +47,6 @@
 			    }
 		    });
 		},
-		drilldown: function () {
-
-		},
 		confirmAppointment: function () {
 
 		}
@@ -49,7 +55,7 @@
 	//Window load!
 	$( window ).load(function(){
 		FullCalendarFrontEnd.loadCalendar();
-
+		CourseFilters.bindCourseInput();
 	});
 
 })( jQuery );
