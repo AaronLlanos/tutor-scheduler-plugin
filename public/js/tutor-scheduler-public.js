75649d872fb41f73f2262450384e3afe70914c00
Aaron Llanos <aaronllanos@gmail.com>
2015-08-30 03:55:35 -0500

    Calendar Filters Work

--- public/js/tutor-scheduler-public.js
+++ public/js/tutor-scheduler-public.js
@@ -1,27 +1,17 @@
 (function( $ ) {
 	'use strict';
 
-	//Course input functions
-	var CourseFilters = {
-		bindCourseInput: function () {
-			// $("#course-select").on()
-			var courseNamesList = [];
-			$.each(coursesJSON, function (i, course) {
-				console.log(course);
-				courseNamesList.push(course.name);
-			});
-			$("#course-select").autocomplete({
-				source: courseNamesList
-			});
-		}
-	}
-	
+	var filteredEventJSON = [];
+	/**
+	 * Sidenote: $.each() gaurantees order!!! YAY! :D
+	 */
 
 	//Fullcalendar functions
 	var FullCalendarFrontEnd = {
 		loadCalendar: function () {
 			var self = this;
-			$("#fullcalendar-frontend").fullCalendar({
+			var calendar = $("#fullcalendar-frontend");
+			calendar.fullCalendar({
 		        editable: false,
 		        allDaySlot: false,
 		        header: {
@@ -29,7 +19,9 @@
 				    center: 'title',
 				    right:  'month,agendaWeek,agendaDay'
 				},
-				events: eventJSON,
+				events: function(start, end, timezone, callback){
+					callback(filteredEventJSON);
+				},
 				slotEventOverlap: true,
 		        height: "auto",
 		        minTime: "12:00:00",
@@ -38,7 +30,8 @@
 		        defaultView: 'agendaWeek',
 		        //Drill down view. Month  view to day view, week view to day view 
 		        dayClick: function(date, jsEvent, view) {
-		        	self.drilldown();
+		        	calendar.fullCalendar('changeView', 'agendaDay');
+		        	calendar.fullCalendar('gotoDate', date);
 			    },
 		        //Tooltip to confirm appointment here.
 			    eventClick: function(calEvent, jsEvent, view) {
@@ -48,14 +41,100 @@
 		    });
 		},
 		confirmAppointment: function () {
+			
+		},
+		updateEvents: function (tutorIDs) {
+			var self = this;
+			if (typeof tutorIDs === "object") {
+				var eventsToAdd;
+				filteredEventJSON = [];
+				$.each(tutorIDs, function (i, tutorID){
+					eventsToAdd = _.filter(eventJSON, function(eventObject){
+						return eventObject.tutor_ID === tutorID.id;
+					});
+					$.merge(filteredEventJSON, eventsToAdd)
+				});
+			}else{
+				filteredEventJSON = _.filter(eventJSON, function(eventObject){
+					return eventObject.tutor_ID === tutorIDs;
+				});
+			}
+			$("#fullcalendar-frontend").fullCalendar('refetchEvents');
 
 		}
+	}
+
+	//Course input functions
+	var CustomInputFilters = {
+		loadBindings: function () {
+			var self = this;
+			$("#course-select").on('change', function(){
+				var selectedCourseID = $(this).val();
+				var filteredTutorJSON;
+				if (selectedCourseID === "") {
+					filteredTutorJSON = self.loadTutorNames(-1);
+				}else{
+					filteredTutorJSON = self.loadTutorNames(selectedCourseID);
+				}
+				FullCalendarFrontEnd.updateEvents(filteredTutorJSON);
+			});
+			$("#tutor-select").on('change', function(){
+				var selectedTutorID = $(this).val();
+				FullCalendarFrontEnd.updateEvents(selectedTutorID);
+			});
+		},
+		loadCourseNames: function () {
+			var optionTemplate;
+			var courseList = $("#course-select");
+			var sortedCoursesJSON = coursesJSON;
+			_.sortBy(sortedCoursesJSON, 'name');
+			courseList.append('<option value="" selected>Select a course</option>');
+			$.each(sortedCoursesJSON, function (i, course) {
+				optionTemplate = '<option value="' + course.id + '">' + course.name + '</option>';
+				courseList.append(optionTemplate);
+			});
+		},
+		loadTutorNames: function (courseID) {
+			var optionTemplate;
+			var tutorsList = $("#tutor-select");
+			var filteredTutorJSON = [];
+			tutorsList.html(''); //Clear list
+			//Load all tutors
+			tutorsList.append('<option value="" selected>Select a tutor</option>');
+			if (courseID === (-1)){
+				filteredTutorJSON = tutorsJSON;
+			}else{
+				//Only load the tutors associated with the course ID from C2T
+				
+				var tutorIDs = _.filter(C2TJSON, function(row){
+					return row.course_ID === courseID;
+				});
+
+				var tutorToAdd;
+				$.each(tutorIDs, function (i, tutorID){
+					tutorToAdd = _.find(tutorsJSON, function(tutorObject){
+						return tutorObject.id === tutorID.tutor_ID;
+					});
+					filteredTutorJSON.push(tutorToAdd);
+				});
+			}
+			_.sortBy(filteredTutorJSON, 'first_name');
+			$.each(filteredTutorJSON, function (i, tutor) {
+				optionTemplate = '<option value="' + tutor.id + '">' + tutor.first_name + ' ' + tutor.last_name + '</option>';
+				tutorsList.append(optionTemplate);
+			});
+			
+			return filteredTutorJSON;
+		}
+
 	}
 
 	//Window load!
 	$( window ).load(function(){
 		FullCalendarFrontEnd.loadCalendar();
-		CourseFilters.bindCourseInput();
+		CustomInputFilters.loadCourseNames();
+		CustomInputFilters.loadTutorNames(-1);
+		CustomInputFilters.loadBindings();
 	});
 
 })( jQuery );
