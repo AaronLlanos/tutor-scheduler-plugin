8e88d3c9fd1dbd81dd879b3784282c997512807f
Aaron Llanos <aaronllanos@gmail.com>
2015-08-30 04:21:01 -0500

    Add modal functionality to confirm appointment

---
+++ public/partials/confirm-appointment-modal.php
@@ -0,0 +1,16 @@
+<div class="modal-header">
+	<h3>Confirm Appointment</h3>
+</div>
+<div class="modal-content">
+	<form>
+		<div class="col-sm-6">
+			<input id="first-name" class="form-group" placeholder="First Name" required>
+		</div>
+		<div class="col-sm-6">
+			<input id="last-name" class="form-group" placeholder="Last Name" required>
+		</div>
+	</form>
+</div>
+<div class="modal-footer">
+	
+</div>
